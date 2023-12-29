8e2c149c373e76dc5331ff7fc0d165d3c179d892
haziqmuhammad <haziq.muhammad@fpgberhad.com>
2023-12-29 11:49:07 +0800

    delete todolist

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { HiOutlinePencilAlt, HiOutlineTrash } from "react-icons/hi";
 
-export const TodoList = ({ task }) => {
+export const TodoList = ({ task, toggleCompleted, deleteTodo }) => {
   return (
-    <div className="flex flex-row  font-semibold  justify-between w-[350px] items-center text-lg text-white p-2 bg-blue-300 rounded-md">
-      <p className="">{task.task}</p>
+    <div className="flex flex-row font-semibold justify-between w-[350px] items-center text-lg text-white p-2 rounded-md bg-blue-300">
+      <p
+        onClick={() => toggleCompleted(task.id)}
+        className={task.completed ? " line-through" : ""}
+      >
+        {task.task}
+      </p>
       <div className=" gap-2 flex flex-row">
-        <HiOutlinePencilAlt  className="bg-green-500 w-8 h-8 rounded-full p-2"/>
-        <HiOutlineTrash className=" bg-red-500 w-8 h-8 p-2 rounded-full"/>
+        <HiOutlineTrash
+          className=" bg-red-500 w-8 h-8 p-2 rounded-full cursor-pointer "
+          onClick={() => deleteTodo(task.id)}
+        />
       </div>
     </div>
   );
